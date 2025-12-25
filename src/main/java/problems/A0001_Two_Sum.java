1c3fe3c6e552170e8b31eb0122230e650bb81b6c
sujit <sujitmanmode1997@gmail.com>
2025-12-25 14:47:27 +0530

    first

--- src/main/java/problems/A0001_Two_Sum.java
+++ src/main/java/problems/A0001_Two_Sum.java
@@ -1,4 +1,33 @@
 package problems;
 
 public class A0001_Two_Sum {
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        A0001_Two_Sum solution = new A0001_Two_Sum();
+        int[] result = solution.twoSum(nums, target);
+
+        if (result.length == 2) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No solution found");
+        }
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+
+            }
+        }
+        return new int[]{};
+    }
 }
